5b6d91a5e7881ae0847ccd3473fe3159785594c0
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-11-24 19:59:49 +0100

    ANIMACIONES

--- app/src/main/java/com/example/splash/MainActivity.java
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
     }
+
 }
